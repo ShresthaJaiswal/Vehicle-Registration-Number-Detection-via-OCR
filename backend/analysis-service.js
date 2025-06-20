ce9e11e48718887e8deea1bb235fb2dffabc2a62
Shrestha <shrestha@bounce.com>
2025-06-20 07:25:37 +0530

    chore: Seperate out static serving html

--- backend/analysis-service.js
+++ backend/analysis-service.js
@@ -75,17 +75,7 @@ if (AZURE_VISION_KEY && AZURE_VISION_ENDPOINT) {
 
 const app = express();
 app.use(express.json());
-
-// // Add CORS headers
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
-//     if (req.method === 'OPTIONS') {
-//         return res.status(200).end(); 
-//     }
-// });
+app.use(express.static(path.join(__dirname, '..')));
 
 // Serve the monitoring dashboard
 app.get('/', (req, res) => {
